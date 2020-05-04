9b81e92f10b670b95bca42a0a3bdddd2a9b8b7f8
Aditya Dubey <sinister.dubey@gmail.com>
2020-05-04 09:42:38 +0530

    Fix eslint

---
+++ src/js/test-utils.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import AppContext from './context/app.context';
+
+const contextValue = {
+  isLogin: Boolean,
+  setLogin: jest.fn,
+};
+
+// eslint-disable-next-line react/prop-types
+const AllTheProviders = ({ children }) => (
+  <AppContext.Provider value={contextValue}>
+    {children}
+  </AppContext.Provider>
+);
+
+const customRender = (ui, options) => render(ui, { wrapper: AllTheProviders, ...options });
+
+
+export * from '@testing-library/react';
+
+
+export { customRender as render };
